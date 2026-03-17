fee6eaba4db5dce7cc01859505782123c09535c2
Gustavo Oda <odaguga12@gmail.com>
2026-03-17 00:38:03 -0300

    Adiciona chamada da MainActivity como tela inicial do app

--- app/src/main/java/com/example/navigationbetweenscreens/MainActivity.kt
+++ app/src/main/java/com/example/navigationbetweenscreens/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigationbetweenscreens.screens.LoginScreen
 import com.example.navigationbetweenscreens.ui.theme.NavigationbetweenscreensTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,6 +22,7 @@ class MainActivity : ComponentActivity() {
             NavigationbetweenscreensTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
+                    LoginScreen(modifier = Modifier.padding(innerPadding));
                 }
             }
         }
